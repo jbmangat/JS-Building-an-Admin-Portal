e71af1654cf6cf559a20193167182ae53efa1a20
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-08-18 15:15:56 -0700

    updated page to use fetch to store data on the page

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
 
 // Your Code Here
+async function main(){
+    let res = await fetch('http://localhost:3001/listBooks')
+    let books = await res.json()
+    books.forEach(displayBook)
+}
+
+function displayBook(book){
+    let rootDiv = document.querySelector('#root')
+    
+    let li = document.createElement('li')
+    li.textContent = book.title
+    
+    let input = document.createElement('input')
+    input.value = book.quantity
+
+    let saveButt = document.createElement('button')
+    saveButt.textContent = 'Save'
+
+    saveButt.addEventListener('click', function (){
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+            
+    })
+
+    li.append(input, saveButt)
+    rootDiv.append(li)
+}
+
+main()
